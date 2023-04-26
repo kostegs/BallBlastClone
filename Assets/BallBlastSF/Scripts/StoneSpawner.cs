56a0d83bebb75cad5c2564af314181d8b2e7134f
Kostegs <ksuslin@gmail.com>
2023-04-27 00:52:11 +0300

    Reworked all classes, connected to Stones.
     All classes were classified to their zones of responsibility.

--- Assets/BallBlastSF/Scripts/StoneSpawner.cs
+++ Assets/BallBlastSF/Scripts/StoneSpawner.cs
@@ -10,29 +10,29 @@ public class StoneSpawner : MonoBehaviour
     [SerializeField] private Stone _stonePrefab;
     [SerializeField] private Transform[] _spawnPoints;
     
-    /*private void Spawn()
+    public Stone SpawnStone(int stoneSize, int stoneMaxHitPoints, Vector3 stonePosition)
     {
-        Stone stone = Instantiate(_stonePrefab, _spawnPoints[Random.Range(0, _spawnPoints.Length)].position, Quaternion.identity);
-        stone.SetSize((Stone.Size)Random.Range(1, 4));
-        stone.MaxHitPoints = Random.Range(_stoneMinHitPoints, _stoneMaxHitPoints + 1);
-        Debug.Log($"stone.MaxHitPoints {stone.MaxHitPoints}");
-
-        // Set Max HitPoints in dependence of stone size
-        int decreasePercent = 60 - ((int)(stone.StoneSize) * 20);
-        Debug.Log($"decreasePercent {decreasePercent}");
-
-        stone.MaxHitPoints = stone.MaxHitPoints - (stone.MaxHitPoints * decreasePercent) / 100;
-        Debug.Log($"stone.MaxHitPoints {stone.MaxHitPoints}");
-
-        _amountSpawned++;
-    }*/
+        Stone stone = Instantiate(_stonePrefab, stonePosition, Quaternion.identity);
+        stone.SetSize((Stone.StoneSize)stoneSize);
+        stone.HitPoints = stoneMaxHitPoints;
+                        
+        return stone;
+    }
 
     public Stone SpawnStone(int stoneSize, int stoneMaxHitPoints)
     {
-        Stone stone = Instantiate(_stonePrefab, _spawnPoints[Random.Range(0, _spawnPoints.Length)].position, Quaternion.identity);
-        stone.SetSize((Stone.Size)stoneSize);
-        stone.MaxHitPoints = stoneMaxHitPoints;
-                        
+        Vector3 randomPosition = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
+        Stone stone = SpawnStone(stoneSize, stoneMaxHitPoints, randomPosition);
+
+        return stone;
+    }
+
+    public Stone SpawnStone(int stoneSize, int stoneMaxHitPoints, Vector3 stonePosition, float verticalVelocity, float horizontalDirection)
+    {
+        Stone stone = SpawnStone(stoneSize, stoneMaxHitPoints, stonePosition);
+        stone.AddVerticalVelocity(verticalVelocity);
+        stone.SetHorizontalDirection(horizontalDirection);
+
         return stone;
     }
 
