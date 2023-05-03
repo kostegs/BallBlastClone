5f498f0906341647bc1e4f9f397c9cfc71c1fab0
Kostegs <ksuslin@gmail.com>
2023-05-03 08:49:16 +0300

    Improved coins counter; small UI fixes

--- Assets/BallBlastSF/Scripts/Coins/CoinsManager.cs
+++ Assets/BallBlastSF/Scripts/Coins/CoinsManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class CoinsManager : MonoBehaviour
@@ -8,19 +9,25 @@ public class CoinsManager : MonoBehaviour
     [SerializeField] private CoinSpawner _coinSpawner;
     [SerializeField] private StonesManager _stonesManager;
 
+    private int _countOfCoins;
+
     public event Action OnCoinPickedUp;
+
+    public int CountOfCoins { get { return _countOfCoins; } 
+                              set { if (value < 0) _countOfCoins = 0; else _countOfCoins = value; } }
 
     private void Start()
     {
         _stonesManager.OnStoneDestroyed += OnStoneDestroyedHandler;
+        CountOfCoins = DataStorage.CountOfCoins;
+        OnCoinPickedUp?.Invoke();
     }
 
     public void OnCoinCollisionHandler(object coin, CoinCollisionEventArgs eventArgs)
     {
         if (eventArgs._collisionTransform.GetComponent<Cart>() != null)
         {
-            Debug.Log("Turret vs Coin Collision!");
-
+            CountOfCoins++;
             Destroy(((Coin)coin).gameObject);
             OnCoinPickedUp?.Invoke();
         }
@@ -32,6 +39,11 @@ public class CoinsManager : MonoBehaviour
 
         Coin coin = _coinSpawner.SpawnCoin(coinPosition);
         coin.OnCoinCollision += OnCoinCollisionHandler;
+    }
+
+    private void OnDestroy()
+    {
+        DataStorage.FillDataFromCoinsManager(this);
     }
 
 }
