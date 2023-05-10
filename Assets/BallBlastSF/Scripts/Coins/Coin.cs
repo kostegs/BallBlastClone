90c0ea7b5edab386df4a5e4b716b8c731943a7f6
Kostegs <ksuslin@gmail.com>
2023-05-10 17:53:46 +0300

    Fix Balance

--- Assets/BallBlastSF/Scripts/Coins/Coin.cs
+++ Assets/BallBlastSF/Scripts/Coins/Coin.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public event EventHandler<CoinCollisionEventArgs> OnCoinCollision;
+    public int Value { get; private set; }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         CoinCollisionEventArgs eventArgs = new CoinCollisionEventArgs(collision.transform.root);
         OnCoinCollision?.Invoke(this, eventArgs);
+    }
+
+    public void SetValue(int value)
+    {
+        if (value >= 0)
+            Value = value;
     }
 }
